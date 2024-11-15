11ff4def916604f50dd9e0c063a84aaad2abc21a
patrickkenney9801 <patrickkenney9801@gmail.com>
2024-11-15 13:52:51 -0600

    chore: Accept string inputs to read_parquet_dir

--- python/kernmlops/data_import/__init__.py
+++ python/kernmlops/data_import/__init__.py
@@ -3,7 +3,9 @@ from pathlib import Path
 import polars as pl
 
 
-def read_parquet_dir(data_dir: Path, benchmark_name: str | None) -> dict[str, pl.DataFrame]:
+def read_parquet_dir(data_dir: Path | str, benchmark_name: str | None) -> dict[str, pl.DataFrame]:
+    if isinstance(data_dir, str):
+        data_dir = Path(data_dir)
     kernmlops_dfs = dict[str, pl.DataFrame]()
     dataframe_dirs = [x for x in data_dir.iterdir() if x.is_dir()]
     for dataframe_dir in dataframe_dirs:
